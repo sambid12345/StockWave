3f80f9fbfcb808de5e6da53e59012247db6118e1
sambid12345 <108714100+sambid12345@users.noreply.github.com>
2024-04-25 23:38:49 +0530

    main: adding custom validation to password and showing it's strength bar

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
@@ -18,6 +18,8 @@ export class RegisterComponent implements OnInit{
   toastMessage:string = 'from login';
   toastType:string = 'success';
 
+  strengthScore: number = 0;
+
   constructor(
     private fb:FormBuilder,
     private router:Router,
@@ -26,9 +28,10 @@ export class RegisterComponent implements OnInit{
   ngOnInit(): void {
 
     this.registerForm = this.fb.group({
+      
       email:['sam@gmail.com',[Validators.required, Validators.email]],
-      username:['',Validators.required],
-      password:['',Validators.required],
+      username:['',[Validators.required]],
+      password:['',[Validators.required, this.passwordStrengthValidator.bind(this)]],
       role: ['user']
      });
   }
@@ -57,5 +60,39 @@ export class RegisterComponent implements OnInit{
   }
   hideToast(){
     this.showToast = false;
+  }
+
+  passwordStrengthValidator (control: AbstractControl) {
+    const value = control.value;
+
+    // console.log('value', value);
+    
+    const minLength = 6;
+    const hasUppercase = /[A-Z]/.test(value);
+    const hasLowercase = /[a-z]/.test(value);
+    const hasNumber = /\d/.test(value);
+    const hasSpecialChar = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(value);
+
+    let score = 0;
+    if (value.length >= minLength) score++;
+    if (hasUppercase) score++;
+    if (hasLowercase) score++;
+    if (hasNumber) score++;
+    if (hasSpecialChar) score++;
+
+    this.strengthScore = score * 20; // Convert score to percentage
+
+    return score >= 4 ? null : { invalidPassword: true };
+  }
+  getStrengthBarStyle(){
+    let backgroundColor = '';
+    if (this.strengthScore < 30) {
+      backgroundColor = '#e53e3e'; // Red
+    } else if (this.strengthScore >= 30 && this.strengthScore <= 89) {
+      backgroundColor = '#e8e81f'; // Yellow
+    } else {
+      backgroundColor = '#48bb78'; // Green
+    }
+    return { 'background-color': backgroundColor };
   }
 }
