aed66de3010c6e8093388f65666865240a04e6be
sambid12345 <108714100+sambid12345@users.noreply.github.com>
2024-05-09 16:30:40 +0530

    main: adding multi-theme in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,45 +8,55 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"}
+        bgColorSidebar: "var(--color-background-sidebar)",
+        baseBgCol: "var(--color-background-base)",
+        buttonPrimary: "var(--color-button-primary)",
+        cardBgPrimary: "var(--color-button-primary)",
+        btnPrimaryHover:"var(--color-btn-primary-hover)",
+        headerCol: "var(--color-header)",
+        primaryTextCol: "var(--color-primary-text)",
+        secondaryTextCol : "var(--color-secondary-text)",
+        secondaryBadgeCol: "var(--color-badge-secondary)",
+
+        textColPrimary: "var(--color-text-primary)",
       }
     },
-    fontFamily: {
-      'body': [
-    'Inter', 
-    'ui-sans-serif', 
-    'system-ui', 
-    '-apple-system', 
-    'system-ui', 
-    'Segoe UI', 
-    'Roboto', 
-    'Helvetica Neue', 
-    'Arial', 
-    'Noto Sans', 
-    'sans-serif', 
-    'Apple Color Emoji', 
-    'Segoe UI Emoji', 
-    'Segoe UI Symbol', 
-    'Noto Color Emoji'
-  ],
-      'sans': [
-    'Inter', 
-    'ui-sans-serif', 
-    'system-ui', 
-    '-apple-system', 
-    'system-ui', 
-    'Segoe UI', 
-    'Roboto', 
-    'Helvetica Neue', 
-    'Arial', 
-    'Noto Sans', 
-    'sans-serif', 
-    'Apple Color Emoji', 
-    'Segoe UI Emoji', 
-    'Segoe UI Symbol', 
-    'Noto Color Emoji'
-  ]
-    }
+  //   fontFamily: {
+  //     'body': [
+  //   'Inter', 
+  //   'ui-sans-serif', 
+  //   'system-ui', 
+  //   '-apple-system', 
+  //   'system-ui', 
+  //   'Segoe UI', 
+  //   'Roboto', 
+  //   'Helvetica Neue', 
+  //   'Arial', 
+  //   'Noto Sans', 
+  //   'sans-serif', 
+  //   'Apple Color Emoji', 
+  //   'Segoe UI Emoji', 
+  //   'Segoe UI Symbol', 
+  //   'Noto Color Emoji'
+  // ],
+  //     'sans': [
+  //   'Inter', 
+  //   'ui-sans-serif', 
+  //   'system-ui', 
+  //   '-apple-system', 
+  //   'system-ui', 
+  //   'Segoe UI', 
+  //   'Roboto', 
+  //   'Helvetica Neue', 
+  //   'Arial', 
+  //   'Noto Sans', 
+  //   'sans-serif', 
+  //   'Apple Color Emoji', 
+  //   'Segoe UI Emoji', 
+  //   'Segoe UI Symbol', 
+  //   'Noto Color Emoji'
+  // ]
+  //   }
   },
   plugins: [
     require('tailwind-scrollbar'),
