da29edf83ce58605abc0556270188142b1b32be1
sambid12345 <108714100+sambid12345@users.noreply.github.com>
2024-05-24 15:17:19 +0530

    main: designed item details page

--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-item-detail',
   templateUrl: './item-detail.component.html',
   styleUrls: ['./item-detail.component.scss']
 })
-export class ItemDetailComponent {
+export class ItemDetailComponent implements OnInit {
 
+  itemId: string | null = '';
+
+  constructor( private route: ActivatedRoute ){
+
+  }
+  ngOnInit() {
+    this.itemId = this.route.snapshot.paramMap.get('itemId'); // Access the itemId from params
+   
+  }
  
 }
