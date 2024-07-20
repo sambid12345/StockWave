32834da1072706ab943c4f6f1e785edfa05dd2ef
sambid12345 <sambidchampati@gmail.com>
2024-07-20 23:47:18 +0530

    main: adding location dropdown in add location form

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -8,22 +8,26 @@ import { HomeService } from 'src/app/my-home/Service/home.service';
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent implements OnInit, OnChanges {
-  isOpen : boolean = true;
+  isOpen: boolean = true;
   // eyeToggleArr: any;
-  currentPassToggleBtn: any
-  newPassToggleBtn: any 
+  currentPassToggleBtn: any;
+  newPassToggleBtn: any;
   @Output('onModalClose') onModalClose = new EventEmitter<any>();
   @Input('modalInfo') modalInfo!: Modal;
   modalForm: FormGroup = this.fb.group({});
 
-  locationList: any
+  locationTree: any;
+  locationPaths: any;
 
-  constructor(private fb: FormBuilder, private router:Router, private homeService: HomeService){}
-  ngOnInit(): void {
-  }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private homeService: HomeService
+  ) {}
+  ngOnInit(): void {}
 
   ngOnChanges(changes: SimpleChanges): void {
     this.initilizePasswordEyeToggler();
@@ -31,78 +35,140 @@ export class ModalComponent implements OnInit, OnChanges {
     this.getLocationList();
   }
 
-  initilizePasswordEyeToggler(){
-    if(this.modalInfo.hasFormControls && this.modalInfo.popupName === 'changePassword'){
+  initilizePasswordEyeToggler() {
+    if (
+      this.modalInfo.hasFormControls &&
+      this.modalInfo.popupName === 'changePassword'
+    ) {
       this.currentPassToggleBtn = 'password';
       this.newPassToggleBtn = 'password';
     }
   }
- 
-  initilizeModalForm(){
-    if(this.modalInfo.hasFormControls && this.modalInfo.formControlInfo){
-      this.modalInfo.formControlInfo.forEach((frmCtrl: any)=>{
 
-        if(this.modalInfo.popupName === 'changePassword' && frmCtrl.formControlName === 'newPassword'){
-          this.modalForm.addControl('newPassword', new FormControl('', authValidator.passwordStrengthValidator()))
+  initilizeModalForm() {
+    if (this.modalInfo.hasFormControls && this.modalInfo.formControlInfo) {
+      this.modalInfo.formControlInfo.forEach((frmCtrl: any) => {
+        if (
+          this.modalInfo.popupName === 'changePassword' &&
+          frmCtrl.formControlName === 'newPassword'
+        ) {
+          this.modalForm.addControl(
+            'newPassword',
+            new FormControl('', authValidator.passwordStrengthValidator())
+          );
         }
 
-        this.modalForm.addControl(frmCtrl.formControlName,new FormControl(frmCtrl.defaultValue, frmCtrl.validations))
-      })
+        this.modalForm.addControl(
+          frmCtrl.formControlName,
+          new FormControl(frmCtrl.defaultValue, frmCtrl.validations)
+        );
+      });
     }
   }
-  
-  closeModal(btnId?: string){
 
+  closeModal(btnId?: string) {
     console.log('close clicked');
-  
-    if(this.modalInfo.popupName === 'autoLogout'){
-      localStorage.removeItem("authToken");
+
+    if (this.modalInfo.popupName === 'autoLogout') {
+      localStorage.removeItem('authToken');
       localStorage.removeItem('loggeinTimestamp');
       localStorage.removeItem('expiresIn');
-      this.router.navigate(['auth','login']);
+      this.router.navigate(['auth', 'login']);
       this.onModalClose.emit();
-    }else if(this.modalInfo.popupName === 'forgotPassword'){
-      if(btnId === 'sendPassresetBtn' && this.modalInfo.hasFormControls && this.modalForm && this.modalForm.valid && this.modalForm.value){
+    } else if (this.modalInfo.popupName === 'forgotPassword') {
+      if (
+        btnId === 'sendPassresetBtn' &&
+        this.modalInfo.hasFormControls &&
+        this.modalForm &&
+        this.modalForm.valid &&
+        this.modalForm.value
+      ) {
         this.onModalClose.emit(this.modalForm.value);
-      }else{
+      } else {
         this.onModalClose.emit();
       }
-    }else if(this.modalInfo.popupName === 'changePassword'){
-      if( btnId ==='changePassBtn' && this.modalInfo.hasFormControls && this.modalForm && this.modalForm.valid && this.modalForm.value){
+    } else if (this.modalInfo.popupName === 'changePassword') {
+      if (
+        btnId === 'changePassBtn' &&
+        this.modalInfo.hasFormControls &&
+        this.modalForm &&
+        this.modalForm.valid &&
+        this.modalForm.value
+      ) {
         this.onModalClose.emit(this.modalForm.value);
-      }else{
+      } else {
         this.onModalClose.emit();
       }
-    }else if(this.modalInfo.popupName === 'manualLogout'){
-      if(btnId === 'manualLogoutBtn'){
-        this.onModalClose.emit({manualLogout: true});
-      }else{
+    } else if (this.modalInfo.popupName === 'manualLogout') {
+      if (btnId === 'manualLogoutBtn') {
+        this.onModalClose.emit({ manualLogout: true });
+      } else {
         this.onModalClose.emit();
       }
-    }else if(this.modalInfo.popupName === 'createItem'){
-      if( btnId ==='createBtn' && this.modalInfo.hasFormControls && this.modalForm && this.modalForm.valid && this.modalForm.value){
+    } else if (this.modalInfo.popupName === 'createItem') {
+      if (
+        btnId === 'createBtn' &&
+        this.modalInfo.hasFormControls &&
+        this.modalForm &&
+        this.modalForm.valid &&
+        this.modalForm.value
+      ) {
         this.onModalClose.emit(this.modalForm.value);
-      }else{
+      } else {
         this.onModalClose.emit();
       }
-    }else if(this.modalInfo.popupName === 'createLocation'){
-      if( btnId ==='createBtn' && this.modalInfo.hasFormControls && this.modalForm && this.modalForm.valid && this.modalForm.value){
+    } else if (this.modalInfo.popupName === 'createLocation') {
+      if (
+        btnId === 'createBtn' &&
+        this.modalInfo.hasFormControls &&
+        this.modalForm &&
+        this.modalForm.valid &&
+        this.modalForm.value
+      ) {
         this.onModalClose.emit(this.modalForm.value);
-      }else{
+      } else {
         this.onModalClose.emit();
       }
     }
   }
-  getLocationList(){
-    if(this.modalInfo.popupName === 'createLocation'){
-      this.homeService.getLocations().subscribe({
-        next: (locationList: any)=>{
-          this.locationList = locationList;
-        },
-        error: (error: any)=>{
 
-        }
-      })
+  generatePaths(location: any, path: string) {
+    let result: any = [];
+    const currentPath = path ? `${path} -> ${location.name}` : location.name;
+    result.push({
+        locId: location._id,
+        path: currentPath
+    });
+    
+
+    for (let child of location.children) {
+        result = result.concat(this.generatePaths(child, currentPath));
+    }
+    
+    return result;
+  }
+
+  createLocationPaths(locations: any []) {
+    let result : any = [];
+    for (let location of locations) {
+      result = result.concat(this.generatePaths(location, ''));
+    }
+    return result;
+  }
+  getLocationList() {
+    if (this.modalInfo.popupName === 'createLocation') {
+      this.homeService.getLocations().subscribe({
+        next: (locationList: any) => {
+          this.locationTree = locationList;
+
+          console.log('location tree', JSON.stringify(this.locationTree));
+
+          this.locationPaths = this.createLocationPaths(this.locationTree);
+          console.log('location path',this.locationPaths);
+
+        },
+        error: (error: any) => {},
+      });
     }
   }
 }
